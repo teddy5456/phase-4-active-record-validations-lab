0b6efdb8434642fe4ae0cc1652e3c4fa237b6aaa
teddy <teddywambua5456@gmail.com>
2023-06-26 03:44:35 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+    validate :sufficiently_clickbait_y_title
+  
+    private
+  
+    def sufficiently_clickbait_y_title
+      unless title.present? &&
+             (title.include?("Won't Believe") ||
+             title.include?("Secret") ||
+             title.include?("Top") ||
+             title.include?("Guess"))
+        errors.add(:title, "must be sufficiently clickbait-y")
+      end
+    end
+  end
